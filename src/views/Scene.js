9b4683e80c7ff5c5de2970b1a655cab2e8741108
Nikita Nek Dudnik <nikdudnik@gmail.com>
2019-12-01 23:50:50 +0100

    Implement whatever

--- src/views/Scene.js
+++ src/views/Scene.js
@@ -1,42 +1,44 @@
-import React, { useRef, useMemo } from "react";
+import React from "react";
 import * as THREE from "three";
-import { useRender } from "react-three-fiber";
+import { useUpdate } from "react-three-fiber";
 
 function Scene() {
-  const ref = useRef();
-  useRender(() => {
-    ref.current.normalsNeedUpdate = true;
-  }, false);
+  const vertices = new Float32Array([
+    -1.0,
+    -1.0,
+    1.0,
+    1.0,
+    -1.0,
+    1.0,
+    1.0,
+    1.0,
+    1.0,
+
+    1.0,
+    1.0,
+    1.0,
+    -1.0,
+    1.0,
+    1.0,
+    -1.0,
+    -1.0,
+    1.0
+  ]);
+
+  const ref = useUpdate(geometry => {
+    geometry.setAttribute( 'position', new THREE.BufferAttribute( vertices, 3 ) );
+  }, []);
+
   return (
-    <mesh>
-      <geometry
-        ref={ref}
-        normalize={true}
-        computeVertexNormals={true}
-        attach="geometry"
-        vertices={[
-          new THREE.Vector3(0, 0, 0),
-          new THREE.Vector3(1, 0, 0),
-          new THREE.Vector3(1, 1, 0),
-          new THREE.Vector3(0, 1, 0),
-          new THREE.Vector3(0, 0, -1),
-          new THREE.Vector3(1, 0, -1),
-          new THREE.Vector3(1, 1, -1),
-          new THREE.Vector3(0, 1, -1)
-        ]}
-        faces={[
-          new THREE.Face3(0, 1, 2),
-          new THREE.Face3(3, 0, 2),
-          new THREE.Face3(4, 5, 6),
-          new THREE.Face3(7, 4, 6),
-          new THREE.Face3(0, 4, 1),
-          new THREE.Face3(1, 4, 5),
-          new THREE.Face3(3, 7, 2),
-          new THREE.Face3(2, 7, 6)
-        ]}
+    <points>
+      <bufferGeometry ref={ref} attach="geometry" />
+      <pointsMaterial
+        attach="material"
+        color="#FF0000"
+        side={THREE.DoubleSide}
+        size={0.1}
       />
-      <meshNormalMaterial attach="material" side={THREE.DoubleSide} />
-    </mesh>
+    </points>
   );
 }
 
