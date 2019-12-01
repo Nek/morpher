6e18cb175976f0c3d45bc521a4ddc81b8f5a1bd0
Nikita Nek Dudnik <nikdudnik@gmail.com>
2019-12-02 00:03:33 +0100

    Implement something

--- src/views/Scene.js
+++ src/views/Scene.js
@@ -3,41 +3,18 @@ import * as THREE from "three";
 import { useUpdate } from "react-three-fiber";
 
 function Scene() {
-  const vertices = new Float32Array([
-    -1.0,
-    -1.0,
-    1.0,
-    1.0,
-    -1.0,
-    1.0,
-    1.0,
-    1.0,
-    1.0,
-
-    1.0,
-    1.0,
-    1.0,
-    -1.0,
-    1.0,
-    1.0,
-    -1.0,
-    -1.0,
-    1.0
-  ]);
-
-  const ref = useUpdate(geometry => {
-    geometry.setAttribute( 'position', new THREE.BufferAttribute( vertices, 3 ) );
-  }, []);
-
+  const vertices = Array.from({ length: 1800 }).map(() => {
+    const pX = Math.random() * 500 - 250;
+    const pY = Math.random() * 500 - 250;
+    const pZ = Math.random() * 500 - 250;
+    const particle = new THREE.Vector3(pX, pY, pZ);
+    return particle;
+  });
+  console.log(vertices);
   return (
     <points>
-      <bufferGeometry ref={ref} attach="geometry" />
-      <pointsMaterial
-        attach="material"
-        color="#FF0000"
-        side={THREE.DoubleSide}
-        size={0.1}
-      />
+      <geometry attach="geometry" vertices={vertices} />
+      <pointsMaterial attach="material" color="#FF0000" size={20} />
     </points>
   );
 }
