169efabbfe045f6747a156c721d5e88df5d6b26a
Nikita Nek Dudnik <nikdudnik@gmail.com>
2019-12-01 23:05:33 +0100

    Implement phase two

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Canvas } from "react-three-fiber";
 
 import Scene from "./views/Scene";
-import Controls from "./components/Control";
+import Controls from "./components/Controls";
 
 function App() {
   return (
--- src/views/Scene.js
+++ src/views/Scene.js
@@ -1,10 +1,41 @@
-import React from "react";
+import React, { useRef, useMemo } from "react";
+import * as THREE from "three";
+import { useRender } from "react-three-fiber";
 
 function Scene() {
+  const ref = useRef();
+  useRender(() => {
+    ref.current.normalsNeedUpdate = true;
+  }, false);
   return (
     <mesh>
-      <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
-      <meshNormalMaterial attach="material" />
+      <geometry
+        ref={ref}
+        normalize={true}
+        computeVertexNormals={true}
+        attach="geometry"
+        vertices={[
+          new THREE.Vector3(0, 0, 0),
+          new THREE.Vector3(1, 0, 0),
+          new THREE.Vector3(1, 1, 0),
+          new THREE.Vector3(0, 1, 0),
+          new THREE.Vector3(0, 0, -1),
+          new THREE.Vector3(1, 0, -1),
+          new THREE.Vector3(1, 1, -1),
+          new THREE.Vector3(0, 1, -1)
+        ]}
+        faces={[
+          new THREE.Face3(0, 1, 2),
+          new THREE.Face3(3, 0, 2),
+          new THREE.Face3(4, 5, 6),
+          new THREE.Face3(7, 4, 6),
+          new THREE.Face3(0, 4, 1),
+          new THREE.Face3(1, 4, 5),
+          new THREE.Face3(3, 7, 2),
+          new THREE.Face3(2, 7, 6)
+        ]}
+      />
+      <meshNormalMaterial attach="material" side={THREE.DoubleSide} />
     </mesh>
   );
 }
