ba2198f49d6a944ca9287edcc0d7c21f4c163843
Nikita Nek Dudnik <nikdudnik@gmail.com>
2019-12-02 02:01:54 +0100

    Fucking A(nimation)!

--- src/views/Scene.js
+++ src/views/Scene.js
@@ -1,14 +1,16 @@
-import React, { useRef, useMemo } from "react";
+import React, { useRef, useMemo, useState } from "react";
 import * as THREE from "three";
 import { Noise } from "noisejs";
 import { useFrame, useRender } from "react-three-fiber";
 
-var noise = new Noise(1);
+const noise1 = new Noise(1);
+const noise2 = new Noise(5);
 
 const vertices = Array.from({ length: 10000 }).flatMap((_, i) => {
   const pX = Math.floor(i / 100) - 50;
   const pY = (i % 100) - 50;
-  const pZ = noise.perlin2(Math.floor(i / 100) / 10, (i % 100) / 10) * 10 - 100; //-100 + Math.random() * 20;
+  const pZ =
+    noise1.perlin2(Math.floor(i / 100) / 10, (i % 100) / 10) * 10 ; //-100 + Math.random() * 20;
   const particle = [pX, pY, pZ];
   return particle;
 });
@@ -17,30 +19,37 @@ const morphVertices = Array.from({ length: 10000 }).flatMap((_, i) => {
   const pX = Math.floor(i / 100) - 50;
   const pY = (i % 100) - 50;
   const pZ =
-    noise.perlin2(Math.floor(i / 100) / 10, (i % 100) / 10) * 100 - 100; //-100 + Math.random() * 20;
+    noise2.perlin2(Math.floor(i / 100) / 10, (i % 100) / 10) * 100 - 20; //-100 + Math.random() * 20;
   const particle = [pX, pY, pZ];
   return particle;
 });
 
 function Scene() {
+  const [morph, setMorph] = useState(0);
+
+  useFrame(state => {
+    setMorph(Math.sin(state.clock.getElapsedTime()));
+  });
   return (
     <points
-      morphTargetInfluences={[0.5]}
+      morphTargetInfluences={[morph]}
       morphTargetDictionary={{ spikes: morphVertices }}
     >
       <bufferGeometry
         ref={geo => {
-          geo.addAttribute(
-            "position",
-            new THREE.BufferAttribute(new Float32Array(vertices), 3)
-          );
+          if (geo) {
+            geo.setAttribute(
+              "position",
+              new THREE.BufferAttribute(new Float32Array(vertices), 3)
+            );
 
-          geo.morphAttributes.position = [];
-          geo.morphAttributes.position[0] = new THREE.BufferAttribute(
-            new Float32Array(morphVertices),
-            3
-          );
-          geo.morphTargets = [0];
+            geo.morphAttributes.position = [];
+            geo.morphAttributes.position[0] = new THREE.BufferAttribute(
+              new Float32Array(morphVertices),
+              3
+            );
+            geo.morphTargets = [0];
+          }
         }}
         attach="geometry"
       />
